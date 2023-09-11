746309d95be88053040646d21236747a3e412c5f
sebastianvargas-m <72326137+sebastianvargas-m@users.noreply.github.com>
2023-09-11 00:07:05 -0300

    mas weas modificadas

--- america/main.go
+++ america/main.go
@@ -4,57 +4,76 @@ import (
 	"bufio"
 	"context"
 	"fmt"
+	"math"
+	"math/rand"
 	"net"
 	"os"
 	"strconv"
+	"time"
 
 	pb "github.com/GonzaloDiaz300/LAB/america/proto"
 	"google.golang.org/grpc"
 )
+
+var interesados int           // Variable global para modificar los interesados en obtener la key
+var interesados_iniciales = 0 // Variable para almacenar los interesados iniciales y no leer nuevamente el .txt
 
 type america struct {
 	pb.UnimplementedNotificacionServer
 }
 
 func (a *america) Notificar(ctx context.Context, in *pb.NotiReq) (*pb.NotiResp, error) {
-	fmt.Printf("Se envia el 1 de vuelta a la central")
+	fmt.Printf("Se envia el 1 de vuelta a la central\n")
+	crearInteresados(0)
 	return &pb.NotiResp{Respuesta: 1}, nil
 }
 
-func main() {
-	var interesados int
+func crearInteresados(no_registrados int) {
 
-	fileName := "america/parametros_de_inicio.txt"
+	if interesados_iniciales == 0 {
+		fileName := "america/parametros_de_inicio.txt"
 
-	// Intenta abrir el archivo
-	file, err := os.Open(fileName)
-	if err != nil {
-		fmt.Println("Error al abrir el archivo:", err)
-		return
-	}
-	defer file.Close()
-
-	// Leer el contenido de la primera línea
-	scanner := bufio.NewScanner(file)
-	if scanner.Scan() {
-		// Obtener el texto de la primera línea
-		line := scanner.Text()
-
-		// Convertir el texto a un entero
-		intValue, err := strconv.Atoi(line)
+		// Intenta abrir el archivo
+		file, err := os.Open(fileName)
 		if err != nil {
-			fmt.Println("Error al convertir a entero:", err)
+			fmt.Println("Error al abrir el archivo:", err)
 			return
 		}
+		defer file.Close()
 
-		// Almacenar el valor entero globalmente
-		interesados = intValue
+		// Leer el contenido de la primera línea
+		scanner := bufio.NewScanner(file)
+		if scanner.Scan() {
+			// Obtener el texto de la primera línea
+			line := scanner.Text()
+
+			// Convertir el texto a un entero
+			intValue, err := strconv.Atoi(line)
+			if err != nil {
+				fmt.Println("Error al convertir a entero:", err)
+				return
+			}
+			interesados_iniciales = intValue
+			// Almacenar el valor entero globalmente
+		}
+		interesados = interesados_iniciales / 2
+	} else {
+		interesados = (interesados_iniciales - (interesados - no_registrados)) / 2
 	}
+	limiteInferior_interesados := math.Round(float64(interesados) - (float64(interesados) * 0.2))
+	limiteSuperior_interesados := math.Round(float64(interesados) + (float64(interesados) * 0.2))
 
-	// Imprimir el valor almacenado globalmente
+	// Inicializa el generador de números aleatorios con una semilla única
+	rand.Seed(time.Now().UnixNano())
+	// Genera un número aleatorio dentro del rango
+	numeroAleatorio := rand.Intn(int(limiteSuperior_interesados)-int(limiteInferior_interesados)+1) + int(limiteInferior_interesados)
+	fmt.Printf("Número aleatorio dentro del rango [%d, %d]: %d\n", int(limiteInferior_interesados), int(limiteSuperior_interesados), numeroAleatorio)
+
+	interesados = numeroAleatorio
 	fmt.Println("Valor entero global:", interesados)
+}
 
-	/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+func main() {
 
 	listner, err := net.Listen("tcp", ":50051")
 
--- central/main.go
+++ central/main.go
@@ -15,6 +15,10 @@ import (
 	"google.golang.org/grpc"
 )
 
+var contador = 0
+var numero_iteraciones int
+var numero_llaves int
+
 func enviarMensaje(servidor string, mensaje int, wg *sync.WaitGroup) {
 	defer wg.Done()
 	// Aquí puedes implementar la lógica para enviar el mensaje al servidor especificado
@@ -33,52 +37,68 @@ func enviarMensaje(servidor string, mensaje int, wg *sync.WaitGroup) {
 	fmt.Printf("Se recibió %d\n ", res.Respuesta)
 }
 
-func main() {
-	var limiteInferior int
-	var limiteSuperior int
-
-	// Abre el archivo para lectura
-	archivo, err := os.Open("central/parametros_de_inicio.txt")
-	if err != nil {
-		fmt.Println("Error al abrir el archivo:", err)
-		return
-	}
-	defer archivo.Close()
-
-	// Crea un lector para el archivo
-	lector := bufio.NewScanner(archivo)
-
-	// Itera sobre las líneas del archivo
-	for lector.Scan() {
-		// Lee la línea actual
-		linea := lector.Text()
-
-		// Divide la línea en dos partes utilizando "-"
-		partes := strings.Split(linea, "-")
-		// Convierte las partes en enteros
-		int1, err1 := strconv.Atoi(partes[0])
-		int2, err2 := strconv.Atoi(partes[1])
-
-		if err1 != nil || err2 != nil {
-			fmt.Println("Error al convertir a entero en la línea:", linea)
-			continue
-		}
-
-		// Realiza la operación deseada con int1 e int2
-
-		limiteInferior = int1
-		limiteSuperior = int2
-		fmt.Printf("Límite inferior: %d, Límite superior: %d\n", int1, int2)
-	}
-
-	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+func crearLlaves(limiteInferior int, limiteSuperior int) int {
 	// Inicializa el generador de números aleatorios con una semilla única
 	rand.Seed(time.Now().UnixNano())
 	// Genera un número aleatorio dentro del rango
 	numeroAleatorio := rand.Intn(limiteSuperior-limiteInferior+1) + limiteInferior
 
 	fmt.Printf("Número aleatorio dentro del rango [%d, %d]: %d\n", limiteInferior, limiteSuperior, numeroAleatorio)
+	return numeroAleatorio
+}
+
+func main() {
+
+	if contador < 1 {
+		var limiteInferior int
+		var limiteSuperior int
+
+		// Abre el archivo para lectura
+		archivo, err := os.Open("central/parametros_de_inicio.txt")
+		if err != nil {
+			fmt.Println("Error al abrir el archivo:", err)
+			return
+		}
+		defer archivo.Close()
+
+		// Crea un lector para el archivo
+		lector := bufio.NewScanner(archivo)
+
+		// Itera sobre las líneas del archivo
+		if lector.Scan() {
+			// Lee la línea actual
+			linea := lector.Text()
+
+			// Divide la línea en dos partes utilizando "-"
+			partes := strings.Split(linea, "-")
+			// Convierte las partes en enteros
+			int1, err1 := strconv.Atoi(partes[0])
+			int2, err2 := strconv.Atoi(partes[1])
+
+			if err1 != nil || err2 != nil {
+				fmt.Println("Error al convertir a entero en la línea:", linea)
+			}
+
+			// Realiza la operación deseada con int1 e int2
+
+			limiteInferior = int1
+			limiteSuperior = int2
+			fmt.Printf("Límite inferior: %d, Límite superior: %d\n", int1, int2)
+		}
+
+		if lector.Scan() {
+			linea := lector.Text()
+			iteraciones, err := strconv.Atoi(linea)
+			if err != nil {
+				fmt.Println("Error al convertir a entero en la segunda línea:", linea)
+				return
+			}
+			numero_iteraciones = iteraciones
+
+			fmt.Printf("Número interaciones: %d\n", iteraciones)
+		}
+		numero_llaves = crearLlaves(limiteInferior, limiteSuperior)
+	}
 
 	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -86,7 +106,7 @@ func main() {
 	var wg sync.WaitGroup
 	for _, servidor := range servidores {
 		wg.Add(1)
-		go enviarMensaje(servidor, numeroAleatorio, &wg)
+		go enviarMensaje(servidor, numero_llaves, &wg)
 	}
 
 	wg.Wait()
